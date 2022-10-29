379d9406ad038566b4faf81b8a993ed2a224b6c3
Nikos Dimou <nikosdimou2010@gmail.com>
2022-10-29 19:38:02 +0200

    three circles done

--- airfight.py
+++ airfight.py
@@ -13,8 +13,10 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     screen.fill((255,255,255))
-    pygame.draw.circle(screen, (255,0,0),(width/2,height/2),75)
-
+    pygame.draw.circle(screen, (0,0,255),(width/2,height/2),449)
+    pygame.draw.circle(screen, (255,255,255),(width/2,height/2),300)
+    pygame.draw.circle(screen, (0,0,255),(width/2,height/2),126)
+    
     pygame.display.flip()
 
 pygame.quit()
