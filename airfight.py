3a70507f9bd7da1b1b5e8510200f1b3f657a6481
Ioannis Dimou <ioannisdimou@hotmail.com>
2022-12-02 14:49:04 +0100

    now with user input

--- airfight.py
+++ airfight.py
@@ -24,6 +24,26 @@ class Player(pygame.sprite.Sprite):
         self.surf.fill((0, 0, 255))
         self.rect = self.surf.get_rect()
 
+    def update(self, pressed_keys):
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -1)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 1)
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-1, 0)
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(1, 0)
+
+        # Keep player on the screen
+        if self.rect.left < 0:
+            self.rect.left = 0
+        elif self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        elif self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
+
 # initialization code ______________________________________________
 pygame.init()
 
@@ -44,6 +64,12 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    # Get all the keys currently pressed
+    pressed_keys = pygame.key.get_pressed()
+
+    # Update the player sprite based on user keypresses
+    player.update(pressed_keys)
 
     # Fill the background with white        
     screen.fill((255,255,255))
